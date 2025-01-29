8cef06a4ae00675233494d2fea31603c7e39c4d7
Frank Krick <frank.krick@gmail.com>
2025-01-29 15:09:01 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <thread>
+#include <vector>
+#include <wp/wp.h>
+
+struct Port {
+  std::string alias;
+};
+
+struct WirePlumberControl {
+  GOptionContext *context = nullptr;
+  GMainLoop *loop = nullptr;
+  WpCore *core = nullptr;
+  WpObjectManager *om = nullptr;
+};
+
+std::vector<Port> iterate(WpObjectManager *om) {
+  std::vector<Port> result;
+  const auto om_iter = wp_object_manager_new_iterator(om);
+
+  GValue port_value;
+  while (wp_iterator_next(om_iter, &port_value)) {
+    WpPort *port = WP_PORT(g_value_get_object(&port_value));
+    const auto property_value = wp_pipewire_object_get_property(
+        reinterpret_cast<WpPipewireObject *>(port), "port_alias");
+    result.push_back(Port{.alias = std::string(property_value)});
+    g_value_unset(&port_value);
+  }
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  wp_init(WP_INIT_ALL);
+  WirePlumberControl wire_plumber_service;
+
+  wire_plumber_service.context = g_option_context_new("pmx-grpc-api");
+  wire_plumber_service.loop = g_main_loop_new(nullptr, false);
+  wire_plumber_service.core = wp_core_new(nullptr, nullptr, nullptr);
+  if (wp_core_connect(wire_plumber_service.core)) {
+    wire_plumber_service.om = wp_object_manager_new();
+    wp_object_manager_add_interest(wire_plumber_service.om, WP_TYPE_PORT,
+                                   nullptr);
+    wp_object_manager_request_object_features(
+        wire_plumber_service.om, WP_TYPE_GLOBAL_PROXY,
+        WP_PIPEWIRE_OBJECT_FEATURES_MINIMAL);
+
+    wp_core_install_object_manager(wire_plumber_service.core,
+                                   wire_plumber_service.om);
+
+    std::thread invoker([&wire_plumber_service]() {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      iterate(wire_plumber_service.om);
+      iterate(wire_plumber_service.om);
+    });
+
+    g_main_loop_run(wire_plumber_service.loop);
+  }
+}
